b005b77cfcc44a97b99fd97ef1e76a2f9c5bd0f6
alfaa <152788375+penggaris@users.noreply.github.com>
2023-12-04 00:35:02 +0700

    Update Transaksi.java

--- Transaksi.java
+++ Transaksi.java
@@ -1,16 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Objects;
 
-/**
- *
- * @author asus
- */
-class Transaksi {
+public class Transaksi extends User {
+    public static void viewTransaksi(String user) {
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("| Username\t| Nominal\t| Jenis Transaksi\t| Saldo Akhir\t|");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------");
 
-    static void viewTransaksi(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if(user == null){
+            for(int i = 0; i < SeoulbankApp.trx1.length; i++){
+                System.out.println("| "+SeoulbankApp.trx1[i]+"\t\t| "+SeoulbankApp.trx2[i]+"\t\t\t| "+SeoulbankApp.trx3[i]+"\t\t| "+SeoulbankApp.getTrx4()[i]+"\t\t\t| ");
+            }
+        } else{
+            for(int i = 0; i < SeoulbankApp.trx1.length; i++) {
+                if (Objects.equals(SeoulbankApp.trx1[i], user)) {
+                    System.out.println("| " + SeoulbankApp.trx1[i] + "\t\t| " + SeoulbankApp.trx2[i] + "\t\t\t| " + SeoulbankApp.trx3[i] + "\t\t| " + SeoulbankApp.getTrx4()[i] + "\t\t\t| ");
+                }
+            }
+        }
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------");
+        System.out.print("Tekan enter untuk kembali ke halaman sebelumnya");
+        SiBangApp.input();
+        Admin.viewMenuPage(user);
     }
-    
 }
